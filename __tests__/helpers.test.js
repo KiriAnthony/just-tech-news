078f5caba5caf40a5873ac418fe3315699f20283
Kiri Anthony <Kiriko.anthony@gmail.com>
2022-03-25 22:37:50 -0700

    adding tests for helpers

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -1,5 +1,6 @@
 
-const {format_date} = require('../utils/helpers');
+
+const {format_date, format_plural, format_url} = require('../utils/helpers');
 
 test('format_date() returns a date string', () => {
     const date = new Date('2020-03-20 16:12:03');
@@ -11,5 +12,15 @@ test('format_plural() returns a pluralized word if amount>1', () => {
     const word = 'cat';
     const amount = 3;
 
-    expect(format_plural(word, amount)).toBe('cats');
+    expect(format_plural(word, amount)).toEqual('cats');
+});
+
+test('format_url() returns a simplified url string', () => {
+    const url1 = format_url('http://test.com/page/1');
+    const url2 = format_url('https://www.coolstuff.com/abcdefg/');
+    const url3 = format_url('https://www.google.com?q=hello');
+  
+    expect(url1).toBe('test.com');
+    expect(url2).toBe('coolstuff.com');
+    expect(url3).toBe('google.com');
 });
